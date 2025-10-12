daf4e072a57ae07287250cbf424c386e93145547
aliakcin <aliakcin82@gmail.com>
2025-10-12 02:40:54 -0700

    Initial Commit

---
+++ SmartSchoolV2/SmartSchoolV2App.swift
@@ -0,0 +1,17 @@
+//
+//  SmartSchoolV2App.swift
+//  SmartSchoolV2
+//
+//  Created by  mete akcin on 10/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SmartSchoolV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
